14959fd0f9232d09ce9ee81371f28fa03fded48c
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-10-24 09:15:18 +0545

    created genres list widget

--- lib/constants/my_app_constants.dart
+++ lib/constants/my_app_constants.dart
@@ -1,3 +1,4 @@
 class MyAppConstants {
   static const String movieImage = "https://i.ibb.co/FwTPCCc/Ultra-Linx.jpg";
+  static const List<String> genres = ["Horror", "Action", "Thriller", "Darama"];
 }
