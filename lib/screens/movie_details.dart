4b930810175493f525ea562931a746f165aadfcf
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-10-25 08:38:36 +0545

    implementing the movir details screen

--- lib/screens/movie_details.dart
+++ lib/screens/movie_details.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_statemanagement/constants/my_app_constants.dart';
+import 'package:mvvm_statemanagement/constants/my_app_icons.dart';
 import 'package:mvvm_statemanagement/widgets/cached_image.dart';
+import 'package:mvvm_statemanagement/widgets/movies/favorite_btn.dart';
+import 'package:mvvm_statemanagement/widgets/movies/genres_list_widget.dart';
 
 class MovieDetailsScreen extends StatelessWidget {
   const MovieDetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       body: SafeArea(
         child: Stack(
           children: [
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.45,
+              height: size.height * 0.45,
               width: double.infinity,
               child: CachedImageWidget(
                 imgUrl: MyAppConstants.movieImage,
@@ -29,6 +33,63 @@ class MovieDetailsScreen extends StatelessWidget {
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(top: 25),
+                        child: Material(
+                          child: Column(
+                            children: [
+                              const SizedBox(height: 25),
+                              Text(
+                                "Movie Title",
+                                style: TextStyle(
+                                  fontSize: 28.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              const SizedBox(height: 5.0),
+                              Padding(
+                                padding: const EdgeInsets.all(18.0),
+                                child: Row(
+                                  children: [
+                                    const Icon(
+                                      MyAppIcons.star,
+                                      color: Colors.amber,
+                                      size: 20,
+                                    ),
+                                    const SizedBox(width: 5),
+                                    Text("9/10"),
+                                    Spacer(),
+                                    Text(
+                                      "ReleaseDate",
+                                      style:
+                                          const TextStyle(color: Colors.grey),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 10),
+                                GenresListWidget(),
+                                const SizedBox(height: 15),
+                                Text("overview" * 200,
+                                    textAlign: TextAlign.justify,
+                                    style: const TextStyle(
+                                      fontSize: 18.0,
+                                    )),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).cardColor,
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(6.0),
+                            child: FavoriteBtnWidget(),
+                          ),
+                        ),
                       ),
                     ],
                   )
