a29bb7831f3f1a55dd1e1185e4d5f4c075dd0efb
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-11-03 17:35:04 +0545

    integrating the api

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,24 +1,3 @@
-// import 'dart:convert';
-// import 'dart:developer';
-
-// import 'package:http/http.dart' as http;
-// import 'package:mvvm_statemanagement/constants/api_constants.dart';
-
-// class ApiService {
-//   Future<void> fetchMovies({int page = 1}) async {
-//     final url =
-//         Uri.parse("${ApiConstants.baseUrl}movie/popular?language=en-US&page=1");
-//     final response = http.get(url);
-//     if (response.statusCode == 200) {
-//       final data = jsonDecode(response.body);
-//       log("data $data");
-//     } else {
-//       throw Exception("Failed to load movies: ${response.statusCode}");
-//     }
-//     //movie/popular?language=en-US&page=1' \
-//   }
-// }
-
 import 'dart:convert';
 import 'dart:developer';
 import 'package:http/http.dart' as http;
